1ea5436bc553798778225c1ec70dbddcefcea99f
enzo ROLFO <enzo.rolfo@epfedu.fr>
2025-10-24 10:21:22 +0200

    Création d'un nouveau projet

---
+++ TP1_manipNombresInt_ROLFO_PEREZ-GOUGES/src/tp1_manipnombresint_rolfo_perez/gouges/TP1_manipNombresInt_ROLFO_PEREZGOUGES.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_rolfo_perez.gouges;
+
+/**
+ *
+ * @author enzoa
+ */
+public class TP1_manipNombresInt_ROLFO_PEREZGOUGES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
