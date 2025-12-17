192702de830c1786362883f0b93e2d74be9fa1cd
enzo ROLFO <enzo.rolfo@epfedu.fr>
2025-12-17 10:12:17 +0100

    cellulelumineuse créé

--- lights-off/src/lights/off/CelluleLumineuse.java
+++ lights-off/src/lights/off/CelluleLumineuse.java
@@ -9,5 +9,35 @@ package lights.off;
  * @author enzoa
  */
 public class CelluleLumineuse {
-    
+    private boolean allumee;
+
+    public CelluleLumineuse() {
+        this.allumee = false; // éteinte au départ
+    }
+
+    public boolean estAllumee() {
+        return allumee;
+    }
+
+    public boolean estEteinte() {
+        return !allumee;
+    }
+
+    public void allumer() {
+        allumee = true;
+    }
+
+    public void eteindre() {
+        allumee = false;
+    }
+
+    public void changerEtat() {
+        allumee = !allumee;
+    }
+
+    @Override
+    public String toString() {
+        // utile pour l'affichage en console
+        return allumee ? "X" : ".";
+    }
 }
