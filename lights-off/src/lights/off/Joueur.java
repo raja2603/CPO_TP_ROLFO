811f70bbcf526d0649e7f711d910e99d232a6fd4
enzo ROLFO <enzo.rolfo@epfedu.fr>
2025-12-17 10:14:24 +0100

    joueur créé

--- lights-off/src/lights/off/Joueur.java
+++ lights-off/src/lights/off/Joueur.java
@@ -9,5 +9,27 @@ package lights.off;
  * @author enzoa
  */
 public class Joueur {
-    
+    private final String nom;
+    private int nbCoups;
+
+    public Joueur(String nom) {
+        this.nom = nom;
+        this.nbCoups = 0;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getNbCoups() {
+        return nbCoups;
+    }
+
+    public void incrementerNbCoups() {
+        nbCoups++;
+    }
+
+    public void reinitialiserNbCoups() {
+        nbCoups = 0;
+    }
 }
