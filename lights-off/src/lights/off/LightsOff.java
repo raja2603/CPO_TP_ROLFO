04e93a1e59db0a6bf59d390e3d4373c31bb93d49
enzo ROLFO <enzo.rolfo@epfedu.fr>
2025-12-17 10:25:15 +0100

    classe principale créé lightOff

--- lights-off/src/lights/off/LightsOff.java
+++ lights-off/src/lights/off/LightsOff.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lights.off;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,11 @@ public class LightsOff {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Entrez votre nom : ");
+        String nom = sc.nextLine();
+
+        Partie partie = new Partie(nom);
+        partie.lancerPartie();    
     }
-    
 }
